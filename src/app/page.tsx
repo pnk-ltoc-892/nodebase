d902a26ccb9e7e1df4cf7e33770cc4165bc68b28
pnk-ltoc-892 <rkpankajsingh151@gmail.com>
2026-01-06 00:09:04 +0530

    tRPC Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,65 @@
-import { Button } from '@/components/ui/button'
-import prisma from '@/lib/db'
-import { cn } from '@/lib/utils'
-import React from 'react'
+// For Client Component
+// 'use client'
+
+// import { useTRPC } from "@/trpc/client"
+// import { useQuery } from "@tanstack/react-query";
+
+// const page = () => {
+//   const trpc = useTRPC();
+//   const { data: users } = useQuery(trpc.getUsers.queryOptions())
+//   return (
+//     <div className="bg-red-900 font-extrabold">
+//       {JSON.stringify(users)}
+//     </div>
+//   )
+// }
+
+// export default page
+
+
+// For Server Component
+// import { cn } from '@/lib/utils'
+// import { caller } from '@/trpc/server'
+// import React from 'react'
+// import { Client } from './client'
+
+// const page = async () => {
+//   const users = await caller.getUsers()
+//   const flag = true
+//   return (
+//     <div className={cn("bg-red-900 font-extrabold", flag === true && "text-black")}>
+//       // Data - Server Component - Client Component
+//       // Leveraging Server Component Load Speed 
+//       <Client users={users} />
+//        {/* {JSON.stringify(users)} */}
+//     </div>
+//   )
+// }
+
+// export default page
+
+
+// For Server Component
+
+import { getQueryClient, trpc } from '@/trpc/server'
+import React, { Suspense } from 'react'
+import { Client } from './client'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 
 const page = async () => {
-  const users = await prisma.user.findMany()
-  const flag = true
+  const queryClient = getQueryClient()
+
+  // Prefetch - Server Component leveraged to prefetch fast
+  // Hand it off to client Component
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+ 
   return (
-    <div className={cn("bg-red-900 font-extrabold", flag === true && "text-black")}>
-      {JSON.stringify(users)}
+    <div className="bg-red-900 font-extrabold">
+      <HydrationBoundary state={dehydrate(queryClient)} >
+        <Suspense fallback={<p>Loading...</p>} >
+          <Client />
+        </Suspense>
+      </HydrationBoundary> 
     </div>
   )
 }
