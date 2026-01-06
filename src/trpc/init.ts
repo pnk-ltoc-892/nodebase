a08d0bef470f5e006bf2008190e5d37108d408e0
pnk-ltoc-892 <rkpankajsingh151@gmail.com>
2026-01-07 01:03:08 +0530

    Setup Authentication - Better Auth

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,4 +1,6 @@
-import { initTRPC } from '@trpc/server';
+import { auth } from '@/lib/auth';
+import { initTRPC, TRPCError } from '@trpc/server';
+import { headers } from 'next/headers';
 import { cache } from 'react';
 
 
@@ -24,3 +26,19 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+// Any procedure using protectedProcedure will get 'user' access, if no error is thrown
+export const protectedProcedure = baseProcedure.use(async ({ctx, next}) => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if(!session){
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Unauthorized"
+    })
+  }
+
+  return next({ctx: {...ctx, auth: session}})
+})
