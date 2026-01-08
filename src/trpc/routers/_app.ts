0d583e503686f03182a9554b72de020f551ff681
pnk-ltoc-892 <rkpankajsingh151@gmail.com>
2026-01-08 18:49:40 +0530

    Setup Background Jobs using Inngest

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,30 @@
+import { inngest } from '@/inngest/client';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    // console.log({userId: ctx.auth.user.id});
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany()
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    // Async stuff to be executed
+    // Fetch Something
+    // await new Promise((r) => setTimeout(r, 5000))
 
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id
+    // Process Something
+    // await new Promise((r) => setTimeout(r, 5000))
+
+    // Lets Use Inngest - lets us execute function in while while user being free on client side
+    // Not stuck while the work is being done
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "pnk@mail.com"
       }
     })
-  })
+    return {success: true, messgae: "job Queued"}
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
