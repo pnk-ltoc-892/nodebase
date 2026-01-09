5b8538206202662c50a3625a0aeeddebcc0f4ff7
pnk-ltoc-892 <rkpankajsingh151@gmail.com>
2026-01-09 19:18:51 +0530

    Setup Polar & Integrate BetterAuth, Create Checkout & Billing Portal

---
+++ src/app/(dashboard)/(rest)/subscriptions/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query"
+import { toast } from "sonner"
+
+const page = () => {
+    const trpc = useTRPC()
+    const testAI = useMutation(trpc.testAI.mutationOptions({
+        onSuccess: () => {
+            toast.success("Success")
+        },
+        onError: ({ message }) => {
+            toast.error(message)
+        }
+    }))
+  return (
+    <div>
+      <Button onClick={() => testAI.mutate()}>
+        Click to test Subscription
+      </Button>
+    </div>
+  )
+}
+
+export default page
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import { polar, checkout, portal, usage, webhooks } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -9,5 +11,25 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
-  }
+  },
+  // Plugins
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "ff2c83ed-9517-4d5e-9032-57f186fb38e3",
+              slug: "Nodebase-Pro" // Custom slug for easy reference in Checkout URL, e.g. /checkout/Nodebase-Pro
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true
+        }),
+        portal()
+      ]
+    })
+  ]
 });
