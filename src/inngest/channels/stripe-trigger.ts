76f217fd3f89646af009fbd33db36be07717cdbe
Pankaj Singh <165186164+pnk-ltoc-892@users.noreply.github.com>
2026-01-22 23:46:55 +0530

    Update src/inngest/channels/stripe-trigger.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/inngest/channels/stripe-trigger.ts
+++ src/inngest/channels/stripe-trigger.ts
@@ -1,7 +1,7 @@
 import { channel, topic } from "@inngest/realtime"
 
 
-export const STRIPE_TRIGGER_CHANNEL_NAME = "google-form-trigger-execution"
+export const STRIPE_TRIGGER_CHANNEL_NAME = "stripe-trigger-execution"
 
 export const stripeTriggerChannel = channel(STRIPE_TRIGGER_CHANNEL_NAME)
                                     .addTopic(
