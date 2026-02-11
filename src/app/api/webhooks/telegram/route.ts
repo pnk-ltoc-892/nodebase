63f503f9a8457d84594abd65db99b9ab45845c0e
pnk-ltoc-892 <rkpankajsingh151@gmail.com>
2026-02-11 23:13:38 +0530

    Fix Telegram Webhook

--- src/app/api/webhooks/telegram/route.ts
+++ src/app/api/webhooks/telegram/route.ts
@@ -1,51 +1,54 @@
 import { sendWorkflowExecution } from "@/inngest/utils";
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = "nodejs"
 
 export async function POST(request: NextRequest){
     try {        
         // Decide which workflow to trigger
         const url = new URL(request.url)
-        const workflowId = url.searchParams.get("workflowId")
-
-        if(!workflowId){
-            return NextResponse.json({
-                success: false,
-                error: "Missing required query parameter: workflowId"
-            }, {status: 400})
-        }
+        const workflowId = url.searchParams.get("workflowId") || ""
 
         // Get The Message
         const data = await request.json();
-
-        // console.log("Bot Webhook Triggered\n");
-        // console.log(data);
-        
         const message = data.message;
-        if(!message?.text){
-            return NextResponse.json({ success: true}, {status: 200})
-        }
+
+        if(!workflowId && !message?.text){
+            return NextResponse.json({
+                success: true,
+                message: "Missing required query parameter: workflowId"
+            }, {status: 200})
+        }        
+        
 
         // Fire workflow execution
-        await sendWorkflowExecution({
-            workflowId,
-            initialData: {
-                workflowId,
-                source: "telegram",
-                text: message.text,
-                chatId: message.chat.id,
-                from: message.from,
-                telegram: data
+        queueMicrotask(async () => {
+            try {
+                await sendWorkflowExecution({
+                    workflowId,
+                    initialData: {
+                        workflowId,
+                        source: "telegram",
+                        text: message.text,
+                        chatId: message.chat.id,
+                        from: message.from,
+                        telegram: data
+                    }
+                })
+                
+            } catch (error) {
+                console.error("Workflow execution failed:", error)                
             }
         })
-
+        
+        // Immediate ACK to Telegram
         return NextResponse.json({ success: true }, { status: 200 })
     } 
     catch (error) {
         console.error("Telegram Webhook error: ", error)
         return NextResponse.json({
-            success: false,
-            error: "Failed to process Telegram event"
-        }, {status: 500})
+            success: true,
+            message: "Failed to process Telegram event"
+        }, {status: 200})
     }
 }
